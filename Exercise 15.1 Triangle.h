6ee21db70900f6548d16208c37b9ffab8411fa1f
Yeseul Kim <45150481+YeseulK@users.noreply.github.com>
2018-12-10 09:17:45 +0900

    Create Exercise 15.1 Triangle.h

---
+++ Exercise 15.1 Triangle.h
@@ -0,0 +1,27 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+#include "GeometricObject.h"
+
+class Triangle : public GeometricObject
+{
+public:
+	Triangle();
+	Triangle(double side1, double side2, double side3);
+	Triangle(double side1, double side2, double side3,
+		const string&color, bool filled);
+	double getSide1() const;
+	void setSide1(double);
+	double getSide2() const;
+	void setSide2(double);
+	double getSide3() const;
+	void setSide3(double);
+	double getArea() const;
+	double getPerimeter() const;
+	string toString() const;
+
+private:
+	double side1;
+	double side2;
+	double side3;
+};
+#endif // !TRIANGLE_H
